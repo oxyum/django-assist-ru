3aafcd04f12cb74ea4cd7c2765544805a6362791
Mike Korobov <kmike84@gmail.com>
2010-02-19 00:00:38 +0500

    Замена URL'ов на тестовые

--- assist/conf.py
+++ assist/conf.py
@@ -13,8 +13,18 @@ TEST_MODE = getattr(settings, 'ASSIST_TEST_MODE', False)
 TEST_MODE1_URL = "https://test.assist.ru/shops/purchase.cfm"
 TEST_MODE2_URL = "https://test.assist.ru/shops/cardpayment.cfm"
 TEST_GET_RESULTS_URL = "https://test.assist.ru/results/results.cfm"
-MODE1_URL = "https://secure.assist.ru/shops/purchase.cfm"
-MODE2_URL = "https://secure.assist.ru/shops/cardpayment.cfm"
-GET_RESULTS_URL = "https://secure.assist.ru/results/results.cfm"
+REAL_MODE1_URL = "https://secure.assist.ru/shops/purchase.cfm"
+REAL_MODE2_URL = "https://secure.assist.ru/shops/cardpayment.cfm"
+REAL_GET_RESULTS_URL = "https://secure.assist.ru/results/results.cfm"
+
 CHARGE_URL = "https://secure.assist.ru/postauths/postauth.cfm"
 REFUND_URL = "https://secure.assist.ru/rvr/rvr.cfm"
+
+if TEST_MODE:
+    MODE1_URL = TEST_MODE1_URL
+    MODE2_URL = TEST_MODE2_URL
+    GET_RESULTS_URL = TEST_GET_RESULTS_URL
+else:
+    MODE1_URL = REAL_MODE1_URL
+    MODE2_URL = REAL_MODE2_URL
+    GET_RESULTS_URL = REAL_GET_RESULTS_URL
