becd50e3e35f37cd1ecf43f4c041ce25f05b845a
Mike Korobov <kmike84@gmail.com>
2010-02-19 19:59:43 +0500

    Тест на корректность работы с русскими буквами

--- assist/tests.py
+++ assist/tests.py
@@ -6,12 +6,12 @@ from assist.managers import parse_csv_response
 from assist.forms import AssistMode1Form, AssistMode2Form
 from assist.conf import SHOP_IDP
 
-csv1 = """Order Number;Comment;Paket date\r\n123;test;\r\n345;UNKNOWN;19.02.2010 04:59:14\r\n"""
+csv1 = u"Order Number;Comment;Paket date\r\n123;проверка;\r\n345;UNKNOWN;19.02.2010 04:59:14\r\n".encode('utf8')
 
 class ParsersTest(TestCase):
     def testParseCsvResponse(self):
         results = parse_csv_response(csv1)
-        self.assertEqual(results, [{'OrderNumber':'123', 'Comment':'test'},
+        self.assertEqual(results, [{'OrderNumber':'123', 'Comment':u'проверка'},
                                    {'OrderNumber':'345', 'PacketDate': '2010-02-19 04:59:14'}])
 
 
